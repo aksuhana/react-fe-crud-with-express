3b0caca6fe2cb02992501fdcdf3fb25d99d769f1
System Administrator <root@imarts-MacBook-Air.local>
2020-04-14 09:58:09 +0530

    Code formatted

--- src/components/addUser.js
+++ src/components/addUser.js
@@ -1,167 +1,132 @@
-import React, {
-    useState
-} from "react";
-import UserDataService from "../services/userService";
+import React, { useState } from 'react';
+import UserDataService from '../services/userService';
 
 const AddUser = () => {
-    const initialUserState = {
+	const initialUserState = {
+		first_name: '',
+		last_name: '',
+		email: '',
+		phone: '',
+		is_user_verified: false,
+		is_email_verified: false,
+		is_phone_verified: false
+	};
+	const [ user, setUser ] = useState(initialUserState);
+	const [ submitted, setSubmitted ] = useState(false);
 
-        first_name: "",
-        last_name: "",
-        email: "",
-        phone: "",
-        is_user_verified: false,
-        is_email_verified: false,
-        is_phone_verified: false
-    };
-    const [user, setUser] = useState(initialUserState);
-    const [submitted, setSubmitted] = useState(false);
+	const handleInputChange = (event) => {
+		const { name, value } = event.target;
+		setUser({
+			...user,
+			[name]: value
+		});
+	};
 
-    const handleInputChange = event => {
-        const {
-            name,
-            value
-        } = event.target;
-        setUser({
-            ...user,
-            [name]: value
-        });
-    };
+	const saveUser = () => {
+		console.log(user);
+		var data = {
+			first_name: user.first_name,
+			last_name: user.last_name,
+			email: user.email,
+			phone: user.phone,
+			is_user_verified: user.is_user_verified,
+			is_email_verified: user.is_email_verified,
+			is_phone_verified: user.is_phone_verified
+		};
+		console.log(data);
 
-    const saveUser = () => {
-        console.log(user);
-        var data = {
-            first_name: user.first_name,
-            last_name: user.last_name,
-            email: user.email,
-            phone: user.phone,
-            is_user_verified: user.is_user_verified,
-            is_email_verified: user.is_email_verified,
-            is_phone_verified: user.is_phone_verified,
+		UserDataService.createUser(data)
+			.then((response) => {
+				setUser({
+					id: response.data.id,
+					first_name: response.first_name,
+					last_name: response.last_name,
+					email: response.email,
+					phone: response.phone,
+					is_user_verified: response.is_user_verified,
+					is_email_verified: response.is_email_verified,
+					is_phone_verified: response.is_phone_verified
+				});
+				setSubmitted(true);
+				console.log(response.data);
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	};
 
-        };
-        console.log(data);
+	const newUser = () => {
+		setUser(initialUserState);
+		setSubmitted(false);
+	};
 
-        UserDataService.createUser(data)
-            .then(response => {
-                setUser({
-                    id: response.data.id,
-                    first_name: response.first_name,
-                    last_name: response.last_name,
-                    email: response.email,
-                    phone: response.phone,
-                    is_user_verified: response.is_user_verified,
-                    is_email_verified: response.is_email_verified,
-                    is_phone_verified: response.is_phone_verified,
-                });
-                setSubmitted(true);
-                console.log(response.data);
-            })
-            .catch(e => {
-                console.log(e);
-            });
-    };
-
-    const newUser = () => {
-        setUser(initialUserState);
-        setSubmitted(false);
-    };
-
-    return ( <
-        div className = "submit-form" > {
-            submitted ? ( <
-                div >
-                <
-                h4 > You submitted successfully! < /h4> <
-                button className = "btn btn-success"
-                onClick = {
-                    newUser
-                } >
-                Add User <
-                /button> <
-                /div>
-            ) : ( <
-                div >
-                <
-                div className = "form-group" >
-                <
-                label htmlFor = "title" > First Name < /label> <
-                input type = "text"
-                className = "form-control"
-                id = "fn"
-                required value = {
-                    user.first_name
-                }
-                onChange = {
-                    handleInputChange
-                }
-                name = "first_name" /
-                >
-                <
-                /div> <
-                div className = "form-group" >
-                <
-                label htmlFor = "title" > Last Name < /label> <
-                input type = "text"
-                className = "form-control"
-                id = "ln"
-                required value = {
-                    user.last_name
-                }
-                onChange = {
-                    handleInputChange
-                }
-                name = "last_name" /
-                >
-                <
-                /div> <
-                div className = "form-group" >
-                <
-                label htmlFor = "title" > Email < /label> <
-                input type = "text"
-                className = "form-control"
-                id = "email"
-                required value = {
-                    user.email
-                }
-                onChange = {
-                    handleInputChange
-                }
-                name = "email" /
-                >
-                <
-                /div> <
-                div className = "form-group" >
-                <
-                label htmlFor = "title" > Phone Number < /label> <
-                input type = "text"
-                className = "form-control"
-                id = "phn"
-                required value = {
-                    user.phone
-                }
-                onChange = {
-                    handleInputChange
-                }
-                name = "phone" /
-                >
-                <
-                /div>
-
-
-
-                <
-                button onClick = {
-                    saveUser
-                }
-                className = "btn btn-success" >
-                Submit <
-                /button> <
-                /div>
-            )
-        } <
-        /div>
-    );
+	return (
+		<div className="submit-form">
+			
+			{submitted ? (
+				<div>
+					<h4> You submitted successfully! </h4>
+					<button className="btn btn-success" onClick={newUser}>
+						Add User
+					</button>
+				</div>
+			) : (
+				<div>
+					<div className="form-group">
+						<label htmlFor="title"> First Name </label>
+						<input
+							type="text"
+							className="form-control"
+							id="fn"
+							required
+							value={user.first_name}
+							onChange={handleInputChange}
+							name="first_name"
+						/>
+					</div>
+					<div className="form-group">
+						<label htmlFor="title"> Last Name </label>
+						<input
+							type="text"
+							className="form-control"
+							id="ln"
+							required
+							value={user.last_name}
+							onChange={handleInputChange}
+							name="last_name"
+						/>
+					</div>
+					<div className="form-group">
+						<label htmlFor="title"> Email </label>
+						<input
+							type="text"
+							className="form-control"
+							id="email"
+							required
+							value={user.email}
+							onChange={handleInputChange}
+							name="email"
+						/>
+					</div>
+					<div className="form-group">
+						<label htmlFor="title"> Phone Number </label>
+						<input
+							type="text"
+							className="form-control"
+							id="phn"
+							required
+							value={user.phone}
+							onChange={handleInputChange}
+							name="phone"
+						/>
+					</div>
+					<button onClick={saveUser} className="btn btn-success">
+						Submit
+					</button>
+				</div>
+			)}
+		</div>
+	);
 };
 
-export default AddUser
+export default AddUser;
--- src/components/user.js
+++ src/components/user.js
@@ -1,186 +1,174 @@
-import React, { useState, useEffect } from "react";
-import UserDataService from "../services/userService.js";
+import React, { useState, useEffect } from 'react';
+import UserDataService from '../services/userService.js';
 
-const User = props => {
-    const initialUserState = {
-    
-        first_name: "",
-        last_name: "",
-        email: "",
-        phone: "",
-        is_user_verified: false,
-        is_email_verified: false,
-        is_phone_verified: false
-      };
-  const [currentUser, setCurrentUser] = useState(initialUserState);
-  const [message, setMessage] = useState("");
+const User = (props) => {
+	const initialUserState = {
+		first_name: '',
+		last_name: '',
+		email: '',
+		phone: '',
+		is_user_verified: false,
+		is_email_verified: false,
+		is_phone_verified: false
+	};
+	const [ currentUser, setCurrentUser ] = useState(initialUserState);
+	const [ message, setMessage ] = useState('');
 
-  const getUser = id => {
-    UserDataService.getUserById(id)
-      .then(response => {
-        setCurrentUser(response.data);
-        console.log(response.data);
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  };
+	const getUser = (id) => {
+		UserDataService.getUserById(id)
+			.then((response) => {
+				setCurrentUser(response.data);
+				console.log(response.data);
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	};
 
-  useEffect(() => {
-    getUser(props.match.params.id);
-  }, [props.match.params.id]);
+	useEffect(
+		() => {
+			getUser(props.match.params.id);
+		},
+		[ props.match.params.id ]
+	);
 
-  const handleInputChange = event => {
-    const { name, value } = event.target;
-    setCurrentUser({ ...currentUser, [name]: value });
-  };
+	const handleInputChange = (event) => {
+		const { name, value } = event.target;
+		setCurrentUser({ ...currentUser, [name]: value });
+	};
 
-  const updateVerify = status => {
-    var data = {
-      id: currentUser.id,
-      first_name: currentUser.first_name,
-      last_name: currentUser.last_name,
-      email: currentUser.email,
-      phone:currentUser.phone
-    };
+	const updateVerify = (status) => {
+		var data = {
+			id: currentUser.id,
+			first_name: currentUser.first_name,
+			last_name: currentUser.last_name,
+			email: currentUser.email,
+			phone: currentUser.phone
+		};
 
-    UserDataService.updateUser(currentUser.id, data)
-      .then(response => {
-        setCurrentUser({ ...currentUser, published: status });
-        console.log(response.data);
-      })
-      .catch(e => {
-        console.log(e);
-      });
-      alert('Oops this functioanlity is not available');
-  };
+		UserDataService.updateUser(currentUser.id, data)
+			.then((response) => {
+				setCurrentUser({ ...currentUser, published: status });
+				console.log(response.data);
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+		alert('Oops this functioanlity is not available');
+	};
 
-  const updateUser = () => {
-      
-      console.log(currentUser._id);
-    UserDataService.updateUser(currentUser._id, currentUser)
-      .then(response => {
-        console.log(response.data);
-        setMessage("The user was updated successfully!");
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  };
+	const updateUser = () => {
+		console.log(currentUser._id);
+		UserDataService.updateUser(currentUser._id, currentUser)
+			.then((response) => {
+				console.log(response.data);
+				setMessage('The user was updated successfully!');
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	};
 
-  const deleteUser = () => {
-    UserDataService.deleteUser(currentUser._id)
-      .then(response => {
-        console.log(response.data);
-        props.history.push("/users");
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  };
+	const deleteUser = () => {
+		UserDataService.deleteUser(currentUser._id)
+			.then((response) => {
+				console.log(response.data);
+				props.history.push('/users');
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	};
 
-  return (
+	return (
+		<div>
+			{currentUser ? (
+				<div className="edit-form">
+					<h4>User</h4>
+					<form>
+						<div className="form-group">
+							<label htmlFor="title">First Name</label>
+							<input
+								type="text"
+								className="form-control"
+								id="fn"
+								required
+								value={currentUser.first_name}
+								onChange={handleInputChange}
+								name="first_name"
+							/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="title">Last Name</label>
+							<input
+								type="text"
+								className="form-control"
+								id="ln"
+								required
+								value={currentUser.last_name}
+								onChange={handleInputChange}
+								name="last_name"
+							/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="title">Email</label>
+							<input
+								type="text"
+								className="form-control"
+								id="email"
+								required
+								value={currentUser.email}
+								onChange={handleInputChange}
+								name="email"
+							/>
+						</div>
+						<div className="form-group">
+							<label htmlFor="title">Phone Number</label>
+							<input
+								type="text"
+								className="form-control"
+								id="phn"
+								required
+								value={currentUser.phone}
+								onChange={handleInputChange}
+								name="phone"
+							/>
+						</div>
+						<div className="form-group">
+							<label>
+								<strong>Status:</strong>
+							</label>
+							{currentUser.is_email_verified ? 'Verified' : 'Unverified'}
+						</div>
+					</form>
 
-    
-    <div>
-    {currentUser ? (
-      <div className="edit-form">
-        <h4>User</h4>
-        <form>
-         
-          <div className="form-group">
-          <label htmlFor="title">First Name</label>
-          <input
-            type="text"
-            className="form-control"
-            id="fn"
-            required
-            value={currentUser.first_name}
-            onChange={handleInputChange}
-            name="first_name"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="title">Last Name</label>
-          <input
-            type="text"
-            className="form-control"
-            id="ln"
-            required
-            value={currentUser.last_name}
-            onChange={handleInputChange}
-            name="last_name"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="title">Email</label>
-          <input
-            type="text"
-            className="form-control"
-            id="email"
-            required
-            value={currentUser.email}
-            onChange={handleInputChange}
-            name="email"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="title">Phone Number</label>
-          <input
-            type="text"
-            className="form-control"
-            id="phn"
-            required
-            value={currentUser.phone}
-            onChange={handleInputChange}
-            name="phone"
-          />
-        </div>
-          <div className="form-group">
-            <label>
-              <strong>Status:</strong>
-            </label>
-            {currentUser.is_email_verified ? "Verified" : "Unverified"}
-          </div>
-        </form>
+					{currentUser.is_email_verified ? (
+						<button className="badge badge-primary mr-2" onClick={() => updateVerify(false)}>
+							Unverify Now
+						</button>
+					) : (
+						<button className="badge badge-primary mr-2" onClick={() => updateVerify(true)}>
+							Verify Now
+						</button>
+					)}
 
-        {currentUser.is_email_verified ? (
-          <button
-            className="badge badge-primary mr-2"
-            onClick={() => updateVerify(false)}
-          >
-            Unverify Now
-          </button>
-        ) : (
-          <button
-            className="badge badge-primary mr-2"
-            onClick={() => updateVerify(true)}
-          >
-            Verify Now
-          </button>
-        )}
+					<button className="badge badge-danger mr-2" onClick={deleteUser}>
+						Delete
+					</button>
 
-        <button className="badge badge-danger mr-2" onClick={deleteUser}>
-          Delete
-        </button>
-
-        <button
-          type="submit"
-          className="badge badge-success"
-          onClick={updateUser}
-        >
-          Update
-        </button>
-        <p>{message}</p>
-      </div>
-    ) : (
-      <div>
-        <br />
-        <p>Please click on a User...</p>
-      </div>
-    )}
-  </div>
-  );
+					<button type="submit" className="badge badge-success" onClick={updateUser}>
+						Update
+					</button>
+					<p>{message}</p>
+				</div>
+			) : (
+				<div>
+					<br />
+					<p>Please click on a User...</p>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default User;
